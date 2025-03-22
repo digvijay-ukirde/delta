803d84140d97b6a5d3be8da476291f344a022738
Digvijay Ukirde <digvijay.ukirde2@ibm.com>
2025-03-22 12:26:56 -0400

    Feature: Alter if change is bigger than threshold

--- config.py
+++ config.py
@@ -4,24 +4,28 @@ PRODUCTS = [
         "name": "Ethereum",
         "symbol": "ETHUSD",
         "resolution": "5m",
+        "candles": 2,
         "change": 40
     },
     {
         "name": "Bitcoin",
         "symbol": "BTCUSD",
         "resolution": "5m",
-        "change": 100
+        "candles": 2,
+        "change": 50
     },
     {
         "name": "Solana",
         "symbol": "SOLUSD",
         "resolution": "5m",
+        "candles": 2,
         "change": 40
     },
     {
         "name": "Ripple",
         "symbol": "XRPUSD",
         "resolution": "5m",
+        "candles": 2,
         "change": 40
     }
 ]
--- main.py
+++ main.py
@@ -14,22 +14,25 @@ logger = setup_logger('DeltaExchangeAPIClient', log_file='delta.log', log_level=
 def main():
     # Initialize DataFetcher
     data_fetcher = DataFetcher()
+    time_units = {'m': 60, 'h': 3600, 'd': 86400, 'w': 604800}
     while True:
         try:
             # Get live ticker data
             for product in PRODUCTS:
-                # ticker_data = data_fetcher.get_tickers_data(product["symbol"])
+                ticker_data = data_fetcher.get_tickers_data(product["symbol"])
 
-                # if ticker_data:
-                #     json_data = ticker_data["result"]
-                #     mark_price = get_value(json_data, "mark_price")
-                #     print(f"{datetime.now().strftime("%Y:%m:%d:%H:%M:%S")}, Product: {product["name"]}, MarketPrice: {mark_price}")
-                # else:
-                #     print("Could not retrieve live data.")
+                if ticker_data:
+                    json_data = ticker_data["result"]
+                    mark_price = get_value(json_data, "mark_price")
+                    logger.debug(f"{datetime.now().strftime("%Y:%m:%d:%H:%M:%S")}, Product: {product["name"]}, MarketPrice: {mark_price}")
+                else:
+                    print("Could not retrieve live data.")
 
                 # Get live historical candle data
                 current_time = int(time.time())
-                start = current_time - (current_time % 300) 
+                diff = (min((int(product["resolution"][:-1])*product["candles"]+2), 2000) * time_units[product["resolution"][-1]])
+                logger.debug(f"Difference between start and end of time period of candles: {diff}")
+                start = current_time - diff
                 end = current_time
                 params = {
                     "resolution": product["resolution"],
@@ -37,17 +40,24 @@ def main():
                     "start": start,
                     "end": end
                 }
+                logger.debug(f"Params: ", params)
                 historical_data = data_fetcher.get_history_candles_data(params)
-
                 if historical_data["result"]:
-                    print(historical_data["result"])
-                    json_data = historical_data["result"][0]
-                    high = get_value(json_data, "high")
-                    low = get_value(json_data, "low")
-                    open = get_value(json_data, "open")
-                    close = get_value(json_data, "close")
-                    volume = get_value(json_data, "volume")
-                    print(f"{datetime.now().strftime("%Y:%m:%d:%H:%M:%S")}, Product: {product["name"]}, High: {high}, Low:{low}, Open: {open}, Close:{close}, Volume:{volume}")
+                    logger.debug(historical_data["result"])
+                    json_data = historical_data["result"]
+                    for index, candle in enumerate(json_data[:product["candles"]]):
+                        high = get_value(json_data[index], "high")
+                        low = get_value(json_data[index], "low")
+                        open = get_value(json_data[index], "open")
+                        close = get_value(json_data[index], "close")
+                        volume = get_value(json_data[index], "volume")
+                        logger.debug(f"{datetime.now().strftime("%Y:%m:%d:%H:%M:%S")}, Product: {product["name"]}, Candle: {index}, High: {high}, Low:{low}, Open: {open}, Close:{close}, Volume:{volume}")
+
+                    lettest_candle = json_data[0]
+                    change_diff = lettest_candle["high"] - lettest_candle["low"]
+                    logger.debug(f"{datetime.now().strftime("%Y:%m:%d:%H:%M:%S")}, Product: {product["name"]}, Change: {change_diff}")
+                    if change_diff > product["change"]:
+                        print(f"{datetime.now().strftime("%Y:%m:%d:%H:%M:%S")}, Product: {product["name"]}, High: {lettest_candle["high"]}, Low: {lettest_candle["high"]}, Change: {change_diff}")     
                 else:
                     print("Could not retrieve live data. Probably at the end of candle. Sleeping for a minute.")
                     time.sleep(60)
